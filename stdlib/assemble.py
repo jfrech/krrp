dd375447e77f6c1d34b5b97b01f73313044af710
Jonathan Frech <info@jfrech.com>
2019-04-18 22:56:38 +0200

    Refactoring.

--- stdlib/assemble.py
+++ stdlib/assemble.py
@@ -2,18 +2,18 @@ import os
 
 
 module_name_aliases = {
-    'list': ('L',),
-    'tuple': ('T',),
-    'prelude': ('P',),
-    'function': ('F',),
-    'math': ('M',),
+    'list': ('list', 'L'),
+    'tuple': ('tuple', 'T'),
+    'prelude': ('prelude', 'P'),
+    'function': ('function', 'F'),
+    'math': ('math', 'M'),
 }
 
 
 stdlib = os.path.dirname(os.path.realpath(__file__))
 
 template = 'atom_scope_push(ImportedSource, atom_name_new(strdup(%s)), atom_string_newfl(%s));'
-c_escape = lambda s: r'(char []) {%s, 0x00}' % ', '.join(fr'0x{ord(c) & 0xFF:X}' for c in s)
+c_escape = lambda s: r'(char []) {%s, 0x00}' % ', '.join(fr'0x{ord(c) & 0xFF:02X}' for c in s)
 
 with open(stdlib + '/krrp_stdlib.c_fragment', 'w') as krrp_stdlib:
     for _file_name in os.listdir(stdlib):
@@ -24,7 +24,7 @@ with open(stdlib + '/krrp_stdlib.c_fragment', 'w') as krrp_stdlib:
         if extension == '.krrp':
             with open(full_file_name, 'r') as f:
                 module_content = f.read()
-                names = (module_name, ) + module_name_aliases.get(module_name, ())
+                names = module_name_aliases.get(module_name, (module_name,))
 
                 for name in names:
                     krrp_stdlib.write(template % (c_escape(name), c_escape(module_content)) + '\n')
